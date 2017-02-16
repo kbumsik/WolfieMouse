aee9a23942708d770c79b69a152248618ebd60d5
Bumsik Kim <kbumsik@gmail.com>
2017-02-16 14:10:04 -0500

    Deleted unnesecessary script folder

--- scripts/setup/config_after_provisioning.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-ln -s ~/wolfieMouse ~/Desktop/
-sed ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml -e 's:"switch_window_key":"empty":g' -i
-xset s blank
--- scripts/truestudio.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cd /opt/Atollic_TrueSTUDIO_for_ARM_7.1.0/ide && ./TrueSTUDIO
