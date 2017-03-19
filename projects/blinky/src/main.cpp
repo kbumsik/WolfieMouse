0333b07a1f03b464fed33972cc4f53c92378b107
Bumsik Kim <kbumsik@gmail.com>
2017-03-19 10:15:34 -0400

    Enabled FreeRTOS and onButtonPressed event for blinky project

--- projects/blinky/src/main.cpp
+++ projects/blinky/src/main.cpp
@@ -8,69 +8,143 @@
 #include "kb_TCA9545A_i2c_mux.h"
 #include "kb_VL6180X_range_finder.h"
 
+// FreeRTOS
+#include "FreeRTOS.h"
+#include "portable.h" // TODO: Delete
+#include "task.h"
+#include "timers.h"
+#include "queue.h"
+#include "semphr.h"
+#include "event_groups.h"
+
 // User config
 #include "system_config.h"
 #include "encoder.h"
 #include "motor.h"
 
+// Define message base. This is used for KB_DEBUG_MSG()
 #ifdef KB_MSG_BASE
 	#undef KB_MSG_BASE
 	#define KB_MSG_BASE "Main"
 #endif
 
+// Event declarations
+static void on_b1_pressed(void);
+static void on_b2_pressed(void);
+
+// Task declarations
+static void task_blinky(void *pvParameters);
+
+// Task Handlers
+TaskHandle_t task_blinky_handler;
+
+/*******************************************************************************
+ * Main function
+ ******************************************************************************/
 int main(void)
 {
-	// initialize clock and system configuration
+	/* initialize clock and system configuration */
     system_init();
 
-    // Initialize all configured peripherals
+    /* Initialize all configured peripherals */
     peripheral_init();
 
-    // init terminal
-    kb_terminal_init();
-
-    // init peripherals for the LED display
-    hcms_290x_init();
+    /* Initial LED Display message */
     hcms_290x_matrix("STRT");
 
-    // init i2c mux
-    tca9545a_init();
-    tca9545a_select_ch(TCA9545A_CH_0);
-
-    // init vl6180x
-    vl6180x_init();
-    vl6180x_range_mm();
-
-    // init encoder
-    encoder_init();
+    /* Encoder test reading */
     int left = encoder_left_count();
     int right = encoder_right_count();
-    KB_DEBUG_MSG("left encoder: %d\r\n", left);
-    KB_DEBUG_MSG("right encoder: %d\r\n", right);
+    KB_DEBUG_MSG("left encoder: %d\n", left);
+    KB_DEBUG_MSG("right encoder: %d\n", right);
 
-    // init motor
-    motor_init();
-    motor_speed_percent(CH_BOTH, 10);
+    /* Motor test running */
+    // motor_speed_percent(CH_BOTH, 10);
     // motor_start(CH_BOTH);
 
-    kb_gpio_toggle(LED4_PORT, LED4_PIN);
-    // wait for .5 second
-    kb_delay_ms(500);
+    /* Set Button Pressed Events */
+    kb_gpio_init_t GPIO_InitStruct;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = NOPULL;
 
+    kb_gpio_isr_enable(B1_PORT, B1_PIN, &GPIO_InitStruct, RISING_EDGE);
+    kb_gpio_isr_register(B1_PORT, B1_PIN, on_b1_pressed);
+
+    kb_gpio_isr_enable(B2_PORT, B2_PIN, &GPIO_InitStruct, RISING_EDGE);
+    kb_gpio_isr_register(B2_PORT, B2_PIN, on_b2_pressed);
+
+    /* Test blinking */
     kb_gpio_toggle(LED4_PORT, LED4_PIN);
+    kb_delay_ms(500);
+    kb_gpio_toggle(LED4_PORT, LED4_PIN);
+
+    /* Test message */
     trace_puts("Hello ARM World!");
-    kb_terminal_puts("Hello World!\r\n");
+    kb_terminal_puts("Hello World!\n");
 
     // motor_stop(CH_BOTH);
+
+    /* definition and creation of defaultTask */
+    BaseType_t result;
+    result = xTaskCreate(
+            task_blinky,            /* Pointer to the function that implements the task */
+            "Blinky",               /* Text name for the task. This is to facilitate debugging only. It is not used in the scheduler */
+            configMINIMAL_STACK_SIZE, /* Stack depth in words */
+            NULL,                   /* Pointer to a task parameters */
+            1,                      /* The task priority */
+            &task_blinky_handler);  /* Pointer of its task handler, if you don't want to use, you can leave it NULL */
+
+    if (result != pdPASS) {
+        KB_DEBUG_ERROR("Creating task failed!!");
+    }
+    /* Do not put delay function in this section!
+     * Because xTaskCreate will stop systick until the scheduler called */
+    // TODO: check if it is true
+    /* Start scheduler */
+    vTaskStartScheduler();
+    while (1) {
+    }
+}
+
+/*******************************************************************************
+ * Tasks
+ ******************************************************************************/
+void task_blinky(void *pvParameters)
+{
+    portTickType xLastWakeTime;
+    /* Initialize xLastWakeTime for vTaskDelayUntil */
+    /* This variable is updated every vTaskDelayUntil is called */
+    xLastWakeTime = xTaskGetTickCount();
+
     uint32_t seconds = 0;
-    while (1)
-    {
-    	kb_gpio_toggle(LED4_PORT, LED4_PIN);
-        kb_terminal_puts("Blink!\r\n");
-    	kb_delay_us(1000000);
+
+    while (1) {
+        kb_gpio_toggle(LED4_PORT, LED4_PIN);
+        kb_terminal_puts("Blink!\n");
 
         hcms_290x_int(seconds);
         ++seconds;        // Count seconds on the trace device.
         trace_printf("Second %u\n", seconds);
+
+        /* Call this Task explicitly every 1000ms ,NOT Delay for 50ms */
+        vTaskDelayUntil(&xLastWakeTime, (1000 / portTICK_RATE_MS));
     }
+
+    /* It never goes here, but the task should be deleted when it reached here */
+    vTaskDelete(NULL);
+}
+
+/*******************************************************************************
+ * Event handlers
+ ******************************************************************************/
+static void on_b1_pressed(void)
+{
+    trace_puts("B1 Pressed\n");
+    return;
+}
+
+static void on_b2_pressed(void)
+{
+    trace_puts("B2 Pressed\n");
+    return;
 }
