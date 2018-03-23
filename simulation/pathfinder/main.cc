8409f2ed9a40acc63a0c584e786117e5e97a2b10
hyunsoochoi0215 <nchsid@gmail.com>
2018-03-23 16:46:13 -0400

    direction coordinate modified(memory -> Decart)

--- simulation/pathfinder/main.cc
+++ simulation/pathfinder/main.cc
@@ -5,12 +5,9 @@
 #include <deque>
 
 std::vector<Position> pathPos = {
-    Position{13, 1}, Position{13, 2}, Position{12, 2}, Position{12, 1}, Position{13, 1}, Position{14, 1},
-    Position{14, 2}, Position{14, 3}, Position{14, 4}, Position{13, 4}, Position{12, 4}, Position{11, 4},
-    Position{10, 4}, Position{9, 4}, Position{8, 4}, Position{7, 4}, Position{7, 5}, Position{7, 6}
+	Position{5,1}, Position{5,0}, Position{4,0}, Position{3,0}, Position{3,1}, Position{3,2}, Position{3,3}, Position{3,4}, Position{3,5}, Position{3,6}, Position{3,7}, Position{4,7}, Position{5,7}, Position{6,7}
 
-};
-
+}
 Queue<Position> queue;
 
 enum direction
@@ -27,11 +24,18 @@ enum real_move
 	F = 'F',
 	H_F = 'h',
 	smooth_R = 'R',
-	smooth_L = 'L'
+	smooth_L = 'L',
+	R_dia_turn = 'r',
+	L_dia_turn = 'l',
+	diagonal_forward = 'D'
 };
 
-std::deque<enum direction> path;
-std::deque<enum real_move> move;
+std::deque<enum direction> path; // position based path.
+
+std::deque<enum real_move> move; // smooth_path
+
+std::deque<enum real_move> diagonal_move; // diagonal path
+std::deque<enum real_move> temp_move;
 
 int main(void)
 {
@@ -41,13 +45,31 @@ int main(void)
 	Position last;
 	direction path_direction;
 	real_move move_direction;
+	real_move before_direction;
+	real_move d_start_direction;
+	real_move d_end_direction;
+
 	char current_status = V; // V : vertical, H : horizontal
+	int cnt = 0;
+	int d_cnt = 1;
+	int x, y, swap;
 
 	init.row = -1;
 	init.col = -1;
 
 	queue.init();
 
+	for (int i = 0; i < pathPos.size(); i++)
+	{
+		swap = (15 - (pathPos[i].row));
+		pathPos[i].row = (pathPos[i].col);
+		pathPos[i].col = swap;
+	}
+	for (int i = 0; i < pathPos.size(); i++)
+	{
+		printf("position {%d,%d}", pathPos[i].row, pathPos[i].col);
+	}
+	printf("\n");
 	for (int i = 0; i < pathPos.size(); i++)
 	{
 		queue.pushToBack(pathPos[i]);
@@ -58,7 +80,6 @@ int main(void)
 
 		if (init.col == -1 && init.row == -1)
 		{
-
 			init = queue.popFromFront();
 			last = queue.popFromFront();
 		}
@@ -151,16 +172,16 @@ int main(void)
 		}
 	}
 
-	/*
-    pritnf("robot path \n")
-    while(!path.empty())   {
-	printf("%c -> ",path.front());
-	path.pop_front();
-    }
-    printf("\n");
-    */
+	//     printf("robot path \n");
+	//     while(!path.empty())   {
+	// 	printf("%c -> ",path.front());
+	// 	path.pop_front();
+	//     }
+	//     printf("\n");
 
 	///////////////////// I don't delete this for just incase we need it /////////////////////
+
+	//smooth turn//
 
 	while (!path.empty())
 	{
@@ -191,14 +212,127 @@ int main(void)
 		}
 	}
 
-	printf(" robot moving direction \n");
-
 	while (!move.empty())
 	{
 		printf("%c -> ", move.front());
 		move.pop_front();
 	}
 	printf("\n");
+
+	///////////////////// I don't delete this for just incase we need it /////////////////////
+
+	// diagonal move//
+
+	// move.push_back(F);
+	// move.push_back(H_F);
+	// move.push_back(smooth_R);
+	// move.push_back(smooth_L);
+	// move.push_back(smooth_R);
+	// move.push_back(smooth_L);
+	// move.push_back(H_F);
+
+	// while (!move.empty())
+	// {
+	// 	if (move.front() == F)
+	// 	{
+	// 		diagonal_move.push_back(move.front());
+	// 		move.pop_front();
+	// 	}
+	// 	else if (move.front() == H_F)
+	// 	{
+
+	// 		diagonal_move.push_back(H_F); //insert H_F
+	// 		move.pop_front();
+	// 		d_start_direction = move.front();
+	// 		// before_direction = move.front();
+	// 		// d_start_direction = before_direction;
+	// 		while (move.front() == smooth_R || move.front() == smooth_L) // temp_move -> inserting all nearby Left/Right moving direction
+	// 		{
+	// 			temp_move.push_back(move.front());
+	// 			move.pop_front();
+	// 			cnt++;
+	// 		}
+
+	// 		if (cnt == 1) // 100% smooth turn
+	// 		{
+	// 			diagonal_move.push_back(temp_move.front());
+	// 			temp_move.pop_front();
+	// 			//diagonal_move.push_back(H_F);
+	// 			cnt--;
+	// 		}
+	// 		else if (cnt > 1) // various direction
+	// 		{
+	// 			while (cnt >= 1)
+	// 			{
+	// 				before_direction = temp_move.front();
+	// 				temp_move.pop_front();
+	// 				if (before_direction != temp_move.front()) // diagonal case counting
+	// 				{
+	// 					d_cnt++;
+	// 					before_direction = temp_move.front();
+	// 					d_end_direction = before_direction;
+	// 					temp_move.pop_front();
+	// 					cnt--;
+	// 				}
+	// 				else if (before_direction == temp_move.front() && d_cnt != 1) // diagonal case moving direction
+	// 				{
+	// 					diagonal_move.push_back((d_start_direction == smooth_R) ? R_dia_turn : L_dia_turn);
+
+	// 					while (d_cnt != 1)
+	// 					{
+	// 						diagonal_move.push_back(diagonal_forward);
+	// 						d_cnt--;
+	// 					}
+	// 					diagonal_move.push_back(diagonal_forward); // as we start at d_cnt = 1 and ends at d_cnt = 1
+	// 					diagonal_move.push_back((d_end_direction == smooth_R) ? R_dia_turn : L_dia_turn);
+	// 				}
+	// 				else if (before_direction == temp_move.front())
+	// 				{ // smooth turn case
+	// 					if ((diagonal_move.back() != L_dia_turn) && (diagonal_move.back() != R_dia_turn))
+	// 					{
+	// 						diagonal_move.push_back(temp_move.front());
+	// 						temp_move.pop_front(); //////////?????????
+	// 						if (temp_move.empty())
+	// 							diagonal_move.push_back(H_F); //////////?????????
+	// 						cnt--;
+	// 					}
+	// 					else if ((diagonal_move.back()) == (L_dia_turn) || (diagonal_move.back()) == (R_dia_turn))
+	// 					//diagonal case finish
+	// 					{
+	// 						//diagonal_move.push_back(temp_move.front());
+	// 						//temp_move.pop_front(); //////////?????????
+	// 						//diagonal_move.push_back(H_F); //////////?????????
+	// 						cnt--;
+	// 					}
+	// 				}
+
+	// 				else if (temp_move.empty() && d_cnt != 1)
+	// 				{
+	// 					diagonal_move.push_back((d_start_direction == smooth_R) ? R_dia_turn : L_dia_turn);
+	// 					while (d_cnt != 1)
+	// 					{
+	// 						diagonal_move.push_back(diagonal_forward);
+	// 						d_cnt--;
+	// 					}
+	// 					diagonal_move.push_back(diagonal_forward); // as we start at d_cnt = 1 and ends at d_cnt = 1
+	// 					diagonal_move.push_back((d_end_direction == smooth_R) ? R_dia_turn : L_dia_turn);
+	// 				}
+	// 			}
+
+	// 			cnt = 0;
+	// 			d_cnt = 1;
+	// 		}
+	// 	}
+	// }
+
+	// printf(" robot moving direction \n");
+
+	// while (!diagonal_move.empty())
+	// {
+	// 	printf("%c -> ", diagonal_move.front());
+	// 	diagonal_move.pop_front();
+	// }
+	// printf("\n");
 
 	return 0;
 }
