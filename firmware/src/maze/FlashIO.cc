3285393ba4188359330a370d576f7398963e8b4c
Bryant Gonzaga <gonzaga96@gmail.com>
2018-03-24 03:16:39 -0400

    finished writing the flashio files.

--- firmware/src/maze/FlashIO.cc
+++ firmware/src/maze/FlashIO.cc
@@ -15,37 +15,40 @@
 FlashIO::FlashIO()
 {
     /* Initialize Variables */
-    dataStatus = NO_DATA_WRT;
-    flashBuffer = NULL;
+    dataStatus = NO_FLASH_DATA;
     sizeOfBuffer = 0;
-    index = 0;
 }
 
 size_t FlashIO::write(const void *ptr, size_t size, size_t nmemb)
 {
     sizeOfBuffer = nmemb * size;
     uint32_t writeStatus = write_flash( (uint8_t*) ptr, sizeOfBuffer );
+    /* If every thing is ok */
     if (writeStatus == HAL_OK) {
-        
+        dataStatus = NEW_FLASH_DATA;
+        return KB_OK;
     }
-     return
+
+    dataStatus = NO_FLASH_DATA;
+    return writestatus;
 }
 
-size_t FlashIO::read()
+size_t FlashIO::read(uint8_t* readBuffer, uint32_t size)
 {
-    if (flashBuffer == NULL) {
-        flashBuffer = malloc(sizeof(uint8_t) * sizeOfBuffer);
+    /* Check flash was actually written */
+    if (dataStatus == NO_FLASH_DATA) {
+        return KB_ERROR;
     }
-    read_flash(flashBuffer, sizeOfBuffer);
-}
+    /* Check for null pointer */
+    if (readBuffer == NULL) {
+        return KB_ERROR;
+    }
+    /* Check that size matches written size */
+    if (size != sizeOfBuffer) {
+        return KB_ERROR;
+    }
+    /* Read Data from Flash */
+    read_flash(readBuffer, size);
 
-FlashIO::getchar()
-{
-    if (flashBuffer == NULL) {
-        return KB_ERROR;
-    }
-    if (index >= sizeOfBuffer) {
-        return KB_ERROR;
-    }
-     return flashBuffer[index++];
+    return KB_OK;
 }
--- firmware/src/maze/FlashIO.hpp
+++ firmware/src/maze/FlashIO.hpp
@@ -16,29 +16,32 @@
 #include "IOInterface.hpp"
 #include "flash.h"
 
-#define NO_DATA_WRT     0x01
-#define NEW_DATA_WRT    0x02
-#define BUFFER_WRT      0x04
+#define NO_FLASH_DATA   0x01    /* No Data in flash */
+#define NEW_FLASH_DATA  0x02    /* Flash programmed */ 
 
 class FlashIO: public IOInterface
 {
 private:
-    uint8_t* flashBuffer;
     uint32_t sizeOfBuffer;
-    uint32_t index;
     uint8_t dataStatus;
 
 public:
     /**
-     * Class constructor
+     * Class Constructor
      */
     FlashIO();
+
+    /**
+     * Class Deconstructor
+     */
+    ~FlashIO();
 
     /**
      * This function writes the data given to it into flash memory.
      * Since flash must be erased before writing to it, this function
      * will write over previously written data. If you would like to 
      * simply erase the reserved sector of flash then you
+     * 
      * @param ptr
      * @param size
      * @param nmemb
@@ -47,26 +50,17 @@ public:
     size_t write(const void* ptr, size_t size, size_t nmemb);
 
     /**
-     * This function reads from the flash and stores the data in
-     * flash_buffer. Since flash.h only provides a function for
-     * reading blocks of flash, then this function should be called
-     * first, before calling getchar(). Furthermore, since this
-     * data in flash will be stored by write() then we already know
-     * exactly how much data was stored in flash.
-     * @return
+     * This function reads from flash and stores the contents in
+     * readBuffer. Furthermore, it makes sure that the size of the
+     * buffer matches the number of bytes written to flash.
+     * @param readBuffer    pointer to array to hold data
+     * @param size          size of buffer
      */
-     size_t read();
-
-    /**
-     * This function will return a character from the flash_buffer.
-     * It will also increase the index by one. Therefore, one could
-     * read the whole buffer by calling this function in a loop.
-     * @return character from flash_buffer
-     */
-    int getchar();
+    size_t read(uint8_t* readBuffer, uint32_t size);
 
     /* Overidden Methods */
     void open(char *filename, char *mode);
+    int getchar();
 };
 
 #endif /* FLASH_IO_H_ */
