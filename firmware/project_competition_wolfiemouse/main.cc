7e84900e7f017dfdd3a50a2c006cba16ed005731
Bumsik Kim <k.bumsik@gmail.com>
2019-03-02 23:07:12 +0900

    Re-organize FSM table

--- firmware/project_competition_wolfiemouse/main.cc
+++ firmware/project_competition_wolfiemouse/main.cc
@@ -23,10 +23,12 @@
 
 
 // Simple displya tasks
-void task_disp_test1(void);
-void task_disp_test2(void);
-void task_disp_test3(void);
-void task_disp_test4(void);
+static void disp_task_1(void);
+static void disp_task_2(void);
+static void disp_task_3(void);
+static void disp_task_4(void);
+static void disp_task_5(void);
+static void disp_task_6(void);
 
 // Define message base. This is used for KB_DEBUG_MSG()
 #ifdef KB_MSG_BASE
@@ -41,11 +43,9 @@ void task_disp_test4(void);
 // Event declarations
 static void on_b1_pressed(void);
 static void on_b2_pressed(void);
-static void on_task_done(void);
 
 // Task declarations
 static void task_main(void *pvParameters);
-static void task_blinky(void *pvParameters);
 
 /*******************************************************************************
  * local variables
@@ -70,13 +70,12 @@ TaskHandle_t task_range_handler;
  * State machine definitions
  ******************************************************************************/
 // FSM action functions
-static void disp_main(void);
-static void disp_mem(void);
-static void search_to_goal(void);
-static void rush_to_goal(void);
-static void task_print_mem(void);
-static void task_reset_mem(void);
-static void task_do_nothing(void);
+static void task_1(void);
+static void task_2(void);
+static void task_3(void);
+static void task_4(void);
+static void task_5(void);
+static void task_6(void);
 
 // event enum
 enum event {
@@ -85,7 +84,7 @@ enum event {
 
 // state enum
 enum state {
-    main_state, mem, test1, test2, test3, test4
+    state_1, state_2, state_3, state_4, state_5, state_6
 };
 
 // transition and table
@@ -96,73 +95,75 @@ struct transition {
 };
 
 // stopwatch current state
-enum state current_state = main_state;
+enum state current_state = state_1;
 
 // fsm function
 void main_fsm(enum event event_input){
-    static const struct transition main_transitions[] = {
+    static const struct transition state_1_transitions[] = {
     //  Event        Task             Next_state
-        {b1_pressed, search_to_goal,  main_state},
-        {b2_pressed, search_to_goal,    main_state},
-        {wheel_up,   disp_mem,        mem},
-        {wheel_down, task_disp_test4,      test4},
-        {eol,        disp_mem,      main_state}
+        {b1_pressed, task_1,          state_1   },
+        {b2_pressed, task_1,          state_1   },
+        {wheel_up,   disp_task_2,     state_2   },
+        {wheel_down, disp_task_6,     state_6   },
+        {eol,        disp_task_2,     state_1   }
     };
-    static const struct transition mem_transitions[] = {
-    //  Event        Task        Next_state
-        {b1_pressed, task_print_mem,  mem},
-        {b2_pressed, task_reset_mem,  mem},
-        {wheel_up,   task_disp_test1, test1},
-        {wheel_down, disp_main,  main_state},
-        {eol,        disp_mem, mem}
+    static const struct transition state_2_transitions[] = {
+    //  Event        Task           Next_state
+        {b1_pressed, task_2,        state_2     },
+        {b2_pressed, task_2,        state_2     },
+        {wheel_up,   disp_task_3,   state_3     },
+        {wheel_down, disp_task_1,   state_1     },
+        {eol,        disp_task_2,   state_2     }
     };
-    static const struct transition test1_transitions[] = {
-    //  Event        Task        Next_state
-        // {b1_pressed, task_test1_time,  test1},
-        // {b2_pressed, task_test1_time,  test1},
-        {wheel_up,   task_disp_test2, test2},
-        {wheel_down, disp_mem, mem},
-        {eol,        task_disp_test1, test1}
+    static const struct transition state_3_transitions[] = {
+    //  Event        Task           Next_state
+        {b1_pressed, task_3,        state_3     },
+        {b2_pressed, task_3,        state_3     },
+        {wheel_up,   disp_task_4,   state_4     },
+        {wheel_down, disp_task_2,   state_2     },
+        {eol,        disp_task_3,   state_3     }
     };
-    static const struct transition test2_transitions[] = {
-    //  Event        Task        Next_state
-        // {b1_pressed, task_test2_CCW,  test2},
-        // {b2_pressed, task_test2_CW,  test2},
-        {wheel_up,   task_disp_test3,   test3},
-        {wheel_down, task_disp_test1,   test1},
-        {eol,        task_disp_test2, test2}
+    static const struct transition state_4_transitions[] = {
+    //  Event        Task           Next_state
+        {b1_pressed, task_4,        state_4     },
+        {b2_pressed, task_4,        state_4     },
+        {wheel_up,   disp_task_5,   state_5     },
+        {wheel_down, disp_task_3,   state_3     },
+        {eol,        disp_task_4,   state_4     }
     };
-    static const struct transition test3_transitions[] = {
-    //  Event        Task        Next_state
-        // {b1_pressed, task_test3_walls,  test3},
-        // {b2_pressed, task_test3_walls,  test3},
-        {wheel_up,   task_disp_test4,   test4},
-        {wheel_down, task_disp_test2,   test2},
-        {eol,        task_disp_test3, test3}
+    static const struct transition state_5_transitions[] = {
+    //  Event        Task           Next_state
+        {b1_pressed, task_5,        state_5     },
+        {b2_pressed, task_5,        state_5     },
+        {wheel_up,   disp_task_6,   state_6     },
+        {wheel_down, disp_task_4,   state_4     },
+        {eol,        disp_task_5,   state_5     }
     };
-    static const struct transition test4_transitions[] = {
-    //  Event        Task        Next_state
-        // {b1_pressed, task_test1_distance,  test4},
-        // {b2_pressed, task_test1_distance,  test4},
-        {wheel_up,   disp_main,   main_state},
-        {wheel_down, task_disp_test3,   test3},
-        {eol,        task_disp_test4, test4}
+    static const struct transition state_6_transitions[] = {
+    //  Event        Task           Next_state
+        {b1_pressed, task_6,        state_6     },
+        {b2_pressed, task_6,        state_6     },
+        {wheel_up,   disp_task_1,   state_1     },
+        {wheel_down, disp_task_5,   state_5     },
+        {eol,        disp_task_6,   state_6     }
     };
     // FSM table. Since it is const, it will be stored in FLASH
     static const struct transition *fsm_table[6] = {
-        main_transitions,
-        mem_transitions,
-        test1_transitions,
-        test2_transitions,
-        test3_transitions,
-        test4_transitions,
+        state_1_transitions,
+        state_2_transitions,
+        state_3_transitions,
+        state_4_transitions,
+        state_5_transitions,
+        state_6_transitions,
     };
 
     // search for signal
     int i;
     for (i = 0; (fsm_table[current_state][i].event_val != event_input)
         && (fsm_table[current_state][i].event_val != eol); i++){
+        // Do nothing until hit a transition
     };
+
     // call task function and than change state
     fsm_table[current_state][i].task();
     current_state = fsm_table[current_state][i].next_state;
@@ -203,17 +204,6 @@ int main(void)
 
     /* Task creation and definition */
     BaseType_t result;
-//    result = xTaskCreate(
-//            task_blinky,            /* Pointer to the function that implements the task */
-//            "Blinky",               /* Text name for the task. This is to facilitate debugging only. It is not used in the scheduler */
-//            configMINIMAL_STACK_SIZE+500, /* Stack depth in words */
-//            NULL,                   /* Pointer to a task parameters */
-//            configMAX_PRIORITIES,                      /* The task priority */
-//            &task_blinky_handler);  /* Pointer of its task handler, if you don't want to use, you can leave it NULL */
-//    if (result != pdPASS) {
-//        KB_DEBUG_ERROR("Creating task failed!!");
-//    }
-
     result = xTaskCreate(
             task_main,
             "Main",
@@ -239,7 +229,8 @@ int main(void)
  ******************************************************************************/
 static void task_main(void *pvParameters)
 {
-    hcms_290x_matrix("HI  ");
+    // Just to display "TSK1"
+    main_fsm(eol);
 
     portTickType xLastWakeTime;
     /* Initialize xLastWakeTime for vTaskDelayUntil */
@@ -272,57 +263,18 @@ static void task_main(void *pvParameters)
             main_fsm(wheel_down);
             continue;
         }
-        /* keep running */
+
+        /* Call this Task every 1000ms */
         //vTaskDelayUntil(&xLastWakeTime, (1000 / portTICK_RATE_MS));
     }
     /* It never goes here, but the task should be deleted when it reached here */
     vTaskDelete(NULL);
 }
 
-
-static void task_blinky(void *pvParameters)
-{
-    portTickType xLastWakeTime;
-    xLastWakeTime = xTaskGetTickCount();
-
-    uint32_t seconds = 0;
-
-    while (1) {
-//         gpio_toggle(LED1_PORT, LED1_PIN);
-//         gpio_toggle(LED2_PORT, LED2_PIN);
-//         gpio_toggle(LED3_PORT, LED3_PIN);
-//         gpio_toggle(LED4_PORT, LED4_PIN);
-// #ifdef KB_BLACKWOLF
-//         gpio_toggle(LED5_PORT, LED5_PIN);
-//         gpio_toggle(LED6_PORT, LED6_PIN);
-// #endif
-        // terminal_puts("Blink!\n");
-
-        //hcms_290x_int(seconds);
-        ++seconds;        // Count seconds on the trace device.
-        // trace_printf("Second %u\n", seconds);
-
-        /* Call this Task every 1000ms */
-        vTaskDelayUntil(&xLastWakeTime, (3000 / portTICK_RATE_MS));
-    }
-    vTaskDelete(NULL);
-}
-
-
 /*******************************************************************************
  * FSM action function
  ******************************************************************************/
-static void disp_main(void)
-{
-    hcms_290x_matrix("Main");
-}
-
-static void disp_mem(void)
-{
-    hcms_290x_matrix("Mem");
-}
-
-static void search_to_goal(void)
+static void task_1(void)
 {
     /* Give time to get ready */
     hcms_290x_matrix("RDY.");
@@ -346,7 +298,7 @@ static void search_to_goal(void)
     main_fsm(eol);
 }
 
-static void rush_to_goal(void)
+static void task_2(void)
 {
     /* Not implemented */
     hcms_290x_matrix("None");
@@ -354,7 +306,7 @@ static void rush_to_goal(void)
     main_fsm(eol);
 }
 
-static void task_print_mem(void)
+static void task_3(void)
 {
     /* Not implemented */
     hcms_290x_matrix("None");
@@ -362,7 +314,7 @@ static void task_print_mem(void)
     main_fsm(eol);
 }
 
-static void task_reset_mem(void)
+static void task_4(void)
 {
     /* Not implemented */
     hcms_290x_matrix("None");
@@ -370,10 +322,22 @@ static void task_reset_mem(void)
     main_fsm(eol);
 }
 
-static void task_do_nothing(void)
+static void task_5(void)
 {
-    return; // Do Nothing, literally.
+    /* Not implemented */
+    hcms_290x_matrix("None");
+    delay_ms(2000);
+    main_fsm(eol);
 }
+
+static void task_6(void)
+{
+    /* Not implemented */
+    hcms_290x_matrix("None");
+    delay_ms(2000);
+    main_fsm(eol);
+}
+
 /*******************************************************************************
  * Event handlers
  ******************************************************************************/
@@ -391,28 +355,35 @@ static void on_b2_pressed(void)
     return;
 }
 
-static void on_task_done(void)
+/*******************************************************************************
+ * Task display
+ ******************************************************************************/
+static void disp_task_1(void)
 {
-    // xSemaphoreGive(semphr_task);
+    hcms_290x_matrix("TSK1");
 }
 
-
-void task_disp_test1(void)
+static void disp_task_2(void)
 {
-    hcms_290x_matrix("TST1");
+    hcms_290x_matrix("TSK2");
 }
 
-void task_disp_test2(void)
+void disp_task_3(void)
 {
-    hcms_290x_matrix("TST2");
+    hcms_290x_matrix("TSK3");
 }
 
-void task_disp_test3(void)
+void disp_task_4(void)
 {
-    hcms_290x_matrix("TST3");
+    hcms_290x_matrix("TSK4");
 }
 
-void task_disp_test4(void)
+void disp_task_5(void)
 {
-    hcms_290x_matrix("TST4");
+    hcms_290x_matrix("TSK5");
+}
+
+void disp_task_6(void)
+{
+    hcms_290x_matrix("TSK6");
 }
