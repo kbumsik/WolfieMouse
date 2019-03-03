aedcb97dcba84abc4fba2814e6d0efdd561ec8c2
Bumsik Kim <k.bumsik@gmail.com>
2019-03-03 10:44:30 +0000

    Main: merge with maze_solver

--- firmware/project_competition_wolfiemouse/main.cc
+++ firmware/project_competition_wolfiemouse/main.cc
@@ -1,5 +1,4 @@
 // In the same folder
-#include "thread_solver_loop.hpp"
 #include "main_fsm_table.hpp"
 
 // FreeRTOS
@@ -23,6 +22,12 @@
 #include "thread_control_loop.h"
 #include "cmd.h"
 
+// Maze solver
+#include "MouseController.hpp"
+#include "IOInterface.hpp"
+#include "FakeIO.hpp"
+#include "RealMouse.hpp"
+
 // Define message base. This is used for KB_DEBUG_MSG()
 #ifdef KB_MSG_BASE
 #    undef KB_MSG_BASE
@@ -32,16 +37,182 @@
 /*******************************************************************************
  * Function declarations and local variables
  ******************************************************************************/
+// Maze solver
+void _maze_solver_run(void);
+
+// Thread declarations
+static void _thread_main(void *pvParameters);
+
 // Event declarations
 static void _on_b1_pressed(void);
 static void _on_b2_pressed(void);
 
-// Task declarations
-static void _thread_main(void *pvParameters);
-
 // Local variables
 static int _is_b1_pressed = 0;
 static int _is_b2_pressed = 0;
+
+/*******************************************************************************
+ * FSM action function
+ ******************************************************************************/
+void task_1(void)
+{
+    /* Give time to get ready */
+    hcms_290x_matrix("RDY.");
+    for (int i = 0; i < 6; i++) {
+        gpio_toggle(LED2_PORT, LED2_PIN);
+        delay_ms(500);
+    }
+
+    // wait for button pressed
+    while (!_is_b2_pressed) {
+    }
+    _is_b1_pressed = 0;
+
+    hcms_290x_matrix("Go");
+    delay_ms(1000);
+    hcms_290x_matrix("    ");   // We should print empty spaces
+                                // in order to save battery
+
+    /* Go to main algorithm */
+    _maze_solver_run();
+}
+
+void task_2(void)
+{
+    /* Not implemented */
+    hcms_290x_matrix("None");
+    delay_ms(2000);
+}
+
+void task_3(void)
+{
+    /* Not implemented */
+    hcms_290x_matrix("None");
+    delay_ms(2000);
+}
+
+void task_4(void)
+{
+    /* Not implemented */
+    hcms_290x_matrix("None");
+    delay_ms(2000);
+}
+
+void task_5(void)
+{
+    /* Not implemented */
+    hcms_290x_matrix("None");
+    delay_ms(2000);
+}
+
+void task_6(void)
+{
+    /* Not implemented */
+    hcms_290x_matrix("None");
+    delay_ms(2000);
+}
+
+/*******************************************************************************
+ * Maze_solver
+ ******************************************************************************/
+void _maze_solver_run(void)
+{
+    char tmp;
+    // Maze solver state
+    enum {
+        goGoal  = 0,
+        goStart = 1,
+        explore = 2
+    } mouseState = goGoal;
+
+    // Create virtual mouse hardware for simulation
+    static FakeIO fake;
+    static RealMouse realmouse;
+    static MouseController mouse(NULL, &fake, &fake,
+            (FinderInterface*) &realmouse, (MoverInterface*) &realmouse);
+
+    /* First just print maze */
+    //mouse.printMaze();
+
+    while (true) {
+        //Finite State Machine
+        if (mouse.allDestinationsReached()) {
+            switch (mouseState) {
+            case goGoal:
+                mouseState = explore;
+                mouse.makeRandomDest(4);
+                break;
+            case explore:
+                mouseState = goStart;
+                mouse.makeStartAsDest();
+                break;
+            case goStart:
+                mouseState = goGoal;
+                mouse.makeGoalAsDest();
+                break;
+            default:
+                //printf("Invalid state");
+                break;
+            }
+        } else {
+            if (!mouse.scanAndMove(NULL)) {
+                goto end;
+            }
+        }
+    }
+
+end:
+    return;
+}
+
+/*******************************************************************************
+ * Main thread
+ ******************************************************************************/
+static void _thread_main(void *pvParameters)
+{
+    static struct encoder_data counter;
+    static uint32_t current_steps, last_steps;
+
+    hcms_290x_matrix("BOOT");
+
+    /* Initialize xLastWakeTime for vTaskDelayUntil */
+    /* This variable is updated every vTaskDelayUntil is called */
+    // portTickType xLastWakeTime = xTaskGetTickCount();
+
+    encoder_get(&counter, ENCODER_CH_RIGHT);
+    last_steps = counter.right;
+    while (1) {
+        // polling button states
+        if (_is_b1_pressed) {
+            main_fsm_run_task(b1_pressed);
+            _is_b1_pressed = 0;
+            continue;
+        } else if (_is_b2_pressed) {
+            main_fsm_run_task(b2_pressed);
+            _is_b2_pressed = 0;
+            continue;
+        }
+
+        // Polling right wheel state
+        // Rotation of wheel will be used to select a task.
+        encoder_get(&counter, ENCODER_CH_RIGHT);
+        current_steps = counter.right;
+        if (current_steps > (last_steps + 300)) {
+            last_steps = current_steps;
+            main_fsm_run_task(wheel_up);
+            continue;
+        } else if (current_steps < (last_steps - 300)) {
+            last_steps = current_steps;
+            main_fsm_run_task(wheel_down);
+            continue;
+        }
+
+        /* Call this Task every 1000ms */
+        // vTaskDelayUntil(&xLastWakeTime, (1000 / portTICK_RATE_MS));
+    }
+    /* It never goes here, but the task should be deleted when it reached here */
+    vTaskDelete(NULL);
+}
 
 /*******************************************************************************
  * Main function
@@ -97,115 +268,6 @@ int main(void)
     vTaskStartScheduler();
     while (1) {
     }
-}
-
-/*******************************************************************************
- * Tasks
- ******************************************************************************/
-static void _thread_main(void *pvParameters)
-{
-    static struct encoder_data counter;
-    static uint32_t current_steps, last_steps;
-
-    hcms_290x_matrix("BOOT");
-
-    /* Initialize xLastWakeTime for vTaskDelayUntil */
-    /* This variable is updated every vTaskDelayUntil is called */
-    // portTickType xLastWakeTime = xTaskGetTickCount();
-
-    encoder_get(&counter, ENCODER_CH_RIGHT);
-    last_steps = counter.right;
-    while (1) {
-        // polling button states
-        if (_is_b1_pressed) {
-            main_fsm_run_task(b1_pressed);
-            _is_b1_pressed = 0;
-            continue;
-        } else if (_is_b2_pressed) {
-            main_fsm_run_task(b2_pressed);
-            _is_b2_pressed = 0;
-            continue;
-        }
-
-        // Polling right wheel state
-        // Rotation of wheel will be used to select a task.
-        encoder_get(&counter, ENCODER_CH_RIGHT);
-        current_steps = counter.right;
-        if (current_steps > (last_steps + 300)) {
-            last_steps = current_steps;
-            main_fsm_run_task(wheel_up);
-            continue;
-        } else if (current_steps < (last_steps - 300)) {
-            last_steps = current_steps;
-            main_fsm_run_task(wheel_down);
-            continue;
-        }
-
-        /* Call this Task every 1000ms */
-        // vTaskDelayUntil(&xLastWakeTime, (1000 / portTICK_RATE_MS));
-    }
-    /* It never goes here, but the task should be deleted when it reached here */
-    vTaskDelete(NULL);
-}
-
-/*******************************************************************************
- * FSM action function
- ******************************************************************************/
-void task_1(void)
-{
-    /* Give time to get ready */
-    hcms_290x_matrix("RDY.");
-    for (int i = 0; i < 6; i++) {
-        gpio_toggle(LED2_PORT, LED2_PIN);
-        delay_ms(500);
-    }
-    // wait for button pressed
-    while (!_is_b2_pressed) {
-    }
-    _is_b1_pressed = 0;
-
-    hcms_290x_matrix("Go");
-    delay_ms(1000);
-    hcms_290x_matrix("    ");   // We should print empty spaces
-                                // in order to save battery
-
-    /* Go to main algorithm */
-    main_run();
-}
-
-void task_2(void)
-{
-    /* Not implemented */
-    hcms_290x_matrix("None");
-    delay_ms(2000);
-}
-
-void task_3(void)
-{
-    /* Not implemented */
-    hcms_290x_matrix("None");
-    delay_ms(2000);
-}
-
-void task_4(void)
-{
-    /* Not implemented */
-    hcms_290x_matrix("None");
-    delay_ms(2000);
-}
-
-void task_5(void)
-{
-    /* Not implemented */
-    hcms_290x_matrix("None");
-    delay_ms(2000);
-}
-
-void task_6(void)
-{
-    /* Not implemented */
-    hcms_290x_matrix("None");
-    delay_ms(2000);
 }
 
 /*******************************************************************************
