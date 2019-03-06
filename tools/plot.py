d87914183ed4dcc398dc9efd9b9372d1b11ac1f2
Bumsik Kim <k.bumsik@gmail.com>
2019-03-07 07:00:46 +0900

    Log: updated plot.py to visualize

--- tools/plot.py
+++ tools/plot.py
@@ -8,46 +8,37 @@ import correct_json
 
 # Filter labels
 df = pd.read_json("./out_corrected.json")
-short_labels = ["LS", "RS", "LO", "RO", "TTS", "TRS"]
-long_labels = ["Speed_Left", "Speed_Right", "Output_Left", "Output_Right", "T_setpoint", "R_setpoint"]
+short_labels = ["PTS","PRS",
+                "L","R","F",
+                "DL","DR",
+                "SL","SR",
+                "OT","OR",
+                "T"]
+long_labels = ["T_target", "R_target",
+               "Left_distance", "Right_distance", "Front_distance",
+               "Left_travel", "Right_travel",
+               "Left_speed", "Right_speed",
+               "T_output", "R_output",
+               "time"]
 df = df[short_labels]
 df.columns = long_labels
- 
+
 print(df.tail())
 
 # plot
-fig, axes = plt.subplots(nrows=2, ncols=1)
-speeds = df[["Speed_Left", "Speed_Right", "T_setpoint", "R_setpoint"]]
-pwms = df[["Output_Left", "Output_Right"]]  
+fig, axes = plt.subplots(nrows=3, ncols=1)
+pids = df[["T_target", "R_target",
+           "T_output", "R_output",
+           "time"]]
+speed = df[["Left_speed", "Right_speed",
+            "Left_travel", "Right_travel",
+            "time"]]
+range_sensor = df[["Left_distance", "Right_distance", "Front_distance",
+                   "Left_travel", "Right_travel",
+                   "time"]]
 
-speeds.plot(ax=axes[0]) #, ylim=(0,speeds["T_setpoint"].max()+10), 
-pwms.plot(ax=axes[1]) #, ylim=(-10,pwms["Output_Left"].max()), 
+pids.plot(x="time", ax=axes[0]) #, ylim=(0,speeds["T_setpoint"].max()+10), 
+speed.plot(x="time", ax=axes[1]) #, ylim=(-10,pwms["Output_Left"].max()), 
+range_sensor.plot(x="time", ax=axes[2]) #, ylim=(-10,pwms["Output_Left"].max()), 
 # df.plot(secondary_y=["Output_Left", "Output_Right"], mark_right=False)
 plt.show()
-
-
-
-
-
-import matplotlib.pyplot as plt
-import numpy as np
-from pandas import DataFrame
-df = DataFrame(np.random.randn(5, 3), columns=['A', 'B', 'C'])
-
-fig, ax = plt.subplots()
-ax3 = ax.twinx()
-rspine = ax3.spines['right']
-rspine.set_position(('axes', 1.15))
-ax3.set_frame_on(True)
-ax3.patch.set_visible(False)
-fig.subplots_adjust(right=0.7)
-
-df.A.plot(ax=ax, style='b-')
-# same ax as above since it's automatically added on the right
-df.B.plot(ax=ax, style='r-', secondary_y=True)
-df.C.plot(ax=ax3, style='g-')
-
-# add legend --> take advantage of pandas providing us access
-# to the line associated with the right part of the axis
-ax3.legend([ax.get_lines()[0], ax.right_ax.get_lines()[0], ax3.get_lines()[0]],\
-           ['A','B','C'], bbox_to_anchor=(1.5, 0.5))
