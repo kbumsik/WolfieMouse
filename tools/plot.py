3dead8e0c3225a9dea09ff1f922ac47742aabf47
Bumsik Kim <k.bumsik@gmail.com>
2019-03-09 17:50:42 +0900

    Plot.py: Last update

--- tools/plot.py
+++ tools/plot.py
@@ -23,51 +23,41 @@ long_labels = ["T_target", "R_target",
 df = df[short_labels]
 df.columns = long_labels
 df["travel_distance"] = (df["Left_travel"] + df["Right_travel"]) / 2
+df["left_speed_target"] = (df["T_target"] + df["R_target"])
+df["right_speed_target"] = (df["T_target"] - df["R_target"])
+
 
 print(df.tail())
 
 # plot
-fig, axes = plt.subplots(nrows=3, ncols=2)
+fig, axes = plt.subplots(nrows=2, ncols=2)
 
 # Plot data over time
 df.plot(
-    y=["T_target", "R_target", "T_output", "R_output"],
+    y=["Left_speed", "Right_speed", "left_speed_target", "right_speed_target", "T_target", "R_target"],
     x="time",
     ax=axes[0, 0],
-    # ylim=(0,speeds["T_setpoint"].max()+10),
-    )
-df.plot(
-    y=["Left_speed", "Right_speed"],
-    x="time",
-    ax=axes[1, 0],
     # ylim=(-10,pwms["Output_Left"].max()),
     )
 df.plot(
     y=["Left_distance",
         "Right_distance", "Front_distance"],
     x="time",
-    ax=axes[2, 0],
+    ax=axes[1, 0],
     # ylim=(-10,pwms["Output_Left"].max())
     )
 
 # Plot data over distance
 df.plot(
-    y=["T_target", "R_target",
-        "T_output", "R_output"],
+    y=["Left_speed", "Right_speed", "left_speed_target", "right_speed_target", "T_target", "R_target"],
     x="travel_distance",
     ax=axes[0, 1],
     # ylim=(0,speeds["T_setpoint"].max()+10),
     )
 df.plot(
-    y=["Left_speed", "Right_speed"],
-    x="travel_distance",
-    ax=axes[1, 1],
-    # ylim=(-10,pwms["Output_Left"].max()),
-    )
-df.plot(
     y=["Left_distance", "Right_distance", "Front_distance"],
     x="travel_distance",
-    ax=axes[2, 1],
+    ax=axes[1, 1],
     # ylim=(-10,pwms["Output_Left"].max())
     )
 
